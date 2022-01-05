f440d823a1915f610ce59d37a37cf987a564b321
aayushi16dex <47508108+aayushi16dex@users.noreply.github.com>
2022-01-05 12:37:17 +0530

    Update RandomAadharNumber.java

--- src/main/java/uidai/RandomAadharNumber.java
+++ src/main/java/uidai/RandomAadharNumber.java
@@ -12,5 +12,15 @@ abstract class RandomAadharNumber {
 			sb.append(chars.charAt(rnd.nextInt(chars.length())));
 		return sb.toString();
 	}
+	
+	public static String generateUniqueNumber() {
+		int length = 6;
+		String num = "0123456789";
+		Random rnd = new Random();
+		StringBuilder id = new StringBuilder(length);
+		for (int i = 0; i < length; i++)
+			id.append(num.charAt(rnd.nextInt(num.length())));
+		return id.toString();
+	}
 
 }
